11b20739406ab37bfa0f6a5b75316d3d2da5a6b9
slvr.png <128439456+joeunpyeong@users.noreply.github.com>
2023-09-20 18:36:29 +0900

    2023-09-20

--- Study_Kotlin_01/app/src/main/java/com/example/study_kotlin/MainActivity.kt
+++ Study_Kotlin_01/app/src/main/java/com/example/study_kotlin/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,6 +21,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.study_kotlin.ui.theme.Study_KotlinTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,15 +35,55 @@ class MainActivity : ComponentActivity() {
             Study_KotlinTheme {
                 // A surface container using the 'background' color from the theme
                 var clickCount : MutableState<Int> = remember { mutableStateOf(0) }
+                var messageList : SnapshotStateList<Message> = remember { mutableStateListOf<Message>()}
 
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android",clickCount.value,
-                        onClicked = {
-                            Log.d("TAG", "onCreate:클릭됨")
-                            clickCount.value = clickCount.value+1
-                        }
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    Column() {
+
+                        Greeting("Android",clickCount.value,
+                            onClicked = {
+                                Log.d("TAG", "onCreate:클릭됨")
+                                clickCount.value = clickCount.value+1
+                                val newMsg = Message(id = clickCount.value, content = "메세지입니다 ${clickCount.value}")
+                                messageList.add(newMsg)
+                            })
+                        MessageList(messageList, onDeleteClicked = {
+                            Log.d("TAG", "onDeleteClicked: ${it.id}")
+                            messageList.remove(it)
+                        })
+                    }//Column
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageList(messages: List<Message>,onDeleteClicked:(Message)->Unit) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageRow(message,onDeleteClicked)
+        }
+    }
+}
+
+@Composable
+fun MessageRow(msg: Message,onDeleteClicked:(Message)->Unit) {
+
+    Surface(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.LightGray),
+        shadowElevation = 10.dp
+    ){
+        Column (modifier=Modifier.padding(16.dp)){
+            Text(text = "id : ${msg.id} / content: ${msg.content}")
+            Button(onClick = {onDeleteClicked(msg)}) {
+                Text(text = "삭제")
             }
         }
     }
---
+++ Study_Kotlin_01/app/src/main/java/com/example/study_kotlin/Message.kt
@@ -0,0 +1,3 @@
+package com.example.study_kotlin
+
+data class Message(var id:Int, var content:String)
