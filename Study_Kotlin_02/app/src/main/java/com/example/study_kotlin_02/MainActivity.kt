b4c1f1cc66bb799dd8ccafb250b4e7997be8ec20
slvr.png <128439456+joeunpyeong@users.noreply.github.com>
2023-09-20 21:03:37 +0900

    .

--- Study_Kotlin_02/app/src/main/java/com/example/study_kotlin_02/MainActivity.kt
+++ Study_Kotlin_02/app/src/main/java/com/example/study_kotlin_02/MainActivity.kt
@@ -3,44 +3,41 @@ package com.example.study_kotlin_02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.study_kotlin_02.ui.theme.Study_Kotlin_02Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Study_Kotlin_02Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Box(modifier = Modifier
+                .background(color = Color.Gray)
+                .fillMaxWidth()
+                .height(200.dp),
+            ){
+                Text("hello")
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                    contentAlignment = Alignment.BottomCenter
+                ){
+                    Text(text = "232")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Study_Kotlin_02Theme {
-        Greeting("Android")
     }
 }
